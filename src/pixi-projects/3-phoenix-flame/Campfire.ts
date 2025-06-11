1141b87fbc8937d069c4a50214a4916eacd329d5
Michał Terczyński <mterczynski1@gmail.com>
2025-06-11 22:09:56 +0200

    Move consts to settings

--- src/pixi-projects/3-phoenix-flame/Campfire.ts
+++ src/pixi-projects/3-phoenix-flame/Campfire.ts
@@ -1,30 +1,26 @@
 import * as PIXI from "pixi.js";
+import { settings } from "./settings";
 
 export class Campfire extends PIXI.Container {
-	private numberOfParticles = 100;
-	private particleSpeed = 1.2;
-	private campfireWidth = 200;
-	private fireHeight = 200;
-
 	constructor() {
 		super();
-		this.pivot.set(this.campfireWidth / 2);
+		this.pivot.set(settings.campfireWidth / 2);
 		this.generateParticles();
 	}
 
 	private generateParticles() {
 		// Remove old particles
 		this.removeChildren();
-		for (let i = 0; i < this.numberOfParticles; i++) {
+		for (let i = 0; i < settings.particleCount; i++) {
 			const size = Math.random() * 4 + 4;
 			const graphics = new PIXI.Graphics();
 			graphics.beginFill(0xff6600, 0.5 + Math.random() * 0.5);
 			graphics.drawCircle(0, 0, size);
 			graphics.endFill();
-			graphics.x = Math.random() * this.campfireWidth;
-			graphics.y = Math.random() * this.fireHeight;
+			graphics.x = Math.random() * settings.campfireWidth;
+			graphics.y = Math.random() * settings.fireHeight;
 			// Store custom data for animation
-			(graphics as any).vy = Math.random() * 0.5 * this.particleSpeed;
+			(graphics as any).vy = Math.random() * 0.5;
 			(graphics as any).radius = Math.abs(graphics.x);
 			graphics.name = "particle";
 			this.addChild(graphics);
@@ -32,7 +28,7 @@ export class Campfire extends PIXI.Container {
 	}
 
 	updateParticles(timeDelta: number) {
-		const updatedDelta = timeDelta * 12;
+		const updatedDelta = timeDelta * settings.speed;
 		for (const child of this.children) {
 			if (child.name !== "particle") continue;
 			const particle = child as PIXI.Graphics & {
@@ -40,13 +36,12 @@ export class Campfire extends PIXI.Container {
 				radius: number;
 			};
 			particle.position.y -=
-				Math.random() * 0.5 * this.particleSpeed * updatedDelta;
-			particle.alpha -= 0.003 * this.particleSpeed;
-			particle.alpha -= 0.00012 * Math.abs(particle.x - this.campfireWidth / 2);
-			// particle.alpha -=
-			// 	particle.radius * 0.001 * this.particleSpeed * updatedDelta;
+				Math.random() * 0.5 * settings.particleSpeed * updatedDelta;
+			particle.alpha -= 0.003 * settings.particleSpeed;
+			particle.alpha -=
+				0.00012 * Math.abs(particle.x - settings.campfireWidth / 2);
 			if (particle.position.y <= 0) {
-				particle.position.y = this.fireHeight;
+				particle.position.y = settings.fireHeight;
 				particle.alpha = 1;
 			}
 		}
--- src/pixi-projects/3-phoenix-flame/settings.ts
+++ src/pixi-projects/3-phoenix-flame/settings.ts
@@ -1,5 +1,9 @@
 export const settings = {
 	canvasWidth: 960,
 	canvasHeight: 800,
-	particleCount: 100,
+	particleCount: 300,
+	speed: 12, // overall speed
+	particleSpeed: 1.2,
+	campfireWidth: 200,
+	fireHeight: 200,
 };
