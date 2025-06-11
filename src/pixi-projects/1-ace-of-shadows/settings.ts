87afd1cf9fcbae9b3b68fcc3ff78c4872eaf5904
Michał Terczyński <mterczynski1@gmail.com>
2025-06-11 23:02:45 +0200

    Adjust cardStackMaxVisible value

--- src/pixi-projects/1-ace-of-shadows/settings.ts
+++ src/pixi-projects/1-ace-of-shadows/settings.ts
@@ -4,7 +4,7 @@ export const settings = {
 	cardGap: 6,
 	cardWidth: 83,
 	cardHeight: 115,
-	cardStackMaxVisible: 30,
+	cardStackMaxVisible: 100,
 	flightDuration: 2000, // ms
 	transferInterval: 1000, // ms, delay between card transfers
 };
