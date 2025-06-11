c74290402a685351a7b5ddfbe77a0d9c57d092f1
Michał Terczyński <mterczynski1@gmail.com>
2025-06-11 23:45:09 +0200

    Refactor addStatsJs

--- src/pixi-projects/addStatsJs.ts
+++ src/pixi-projects/addStatsJs.ts
@@ -1,17 +1,18 @@
 // Simple wrapper for stats.js for PixiJS projects
 import Stats from "stats.js";
 
-export function addStatsJs() {
-	let stats = document.getElementById("pixi-stats") as any;
-	if (!stats) {
-		stats = new Stats();
+export function addStatsJs(): Stats {
+	let existingStats = document.getElementById("pixi-stats");
+	if (!existingStats) {
+		const stats = new Stats();
 		stats.showPanel(0); // 0: fps
 		stats.dom.id = "pixi-stats";
 		stats.dom.style.position = "fixed";
 		stats.dom.style.left = "0px";
 		stats.dom.style.top = "0px";
-		stats.dom.style.zIndex = 10000;
+		stats.dom.style.zIndex = "10000";
 		document.body.appendChild(stats.dom);
+		return stats;
 	}
-	return stats;
+	return existingStats as any as Stats;
 }
